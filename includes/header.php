7f0787a5ea6aebc5c7f0101a70f39c23ba131d07
David Claassen <17654@csmrouen.net>
2019-02-12 14:11:14 +0100

    Menu Header

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php?accueil">Menu 1</a></li>
+            <li><a href="index.php?produits">Menu 2</a></li>
+            <li><a href="index.php?inscription">Menu 3</a></li>
+            <li><a href="index.php?contact">Menu 4</a></li>
+        </ul>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,15 +1,2 @@
-<ul>
-    <li><a href="index.php?page=truc">Truc</a></li>
-    <li><a href="index.php?page=machin">Machin</a></li>
-    <li><a href="index.php?page=michel">Michel</a></li>
-</ul>
 <?php
 
-if (isset($_GET['page']))
-{
-    $page = $_GET['page'];
-    echo $page;
-}
-else{
-    echo "Message";
-}
