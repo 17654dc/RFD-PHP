7690b6bd49a55620680aa63df032e098124a3247
David Claassen <17654@csmrouen.net>
2019-02-12 16:58:28 +0100

    ajout if pour eviter fausse adresse dans barre adresse

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
+date_default_timezone_set("Europe/Paris");
+include "./includes/header.php";
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+        }
+else{
+    $page = "accueil";
+}
+//$page = isset($_GET['page']) ? $_GET['page'] : "accueil";
+
+//var_dump($page);
+
+$path = "./includes/";
+$contenu = glob($path . "*.inc.php");
+$page = $path . $page . ".inc.php";
+
+if (in_array($page, $contenu)){
+    include $page;
+}
+else {
+    include "./includes/accueil.inc.php";
+}
+//var_dump($contenu);
+
+
+include "./includes/footer.php";
 
